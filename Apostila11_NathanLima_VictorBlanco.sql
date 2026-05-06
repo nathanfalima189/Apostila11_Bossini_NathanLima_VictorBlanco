f116db7e09e64d3ebc6331d9d5b7fb7a433fe903
RAFAEL <rafaellimapapa898@gmail.com>
2026-05-06 10:36:06 -0300

    Exercicio 1.2

--- Apostila11_NathanLima_VictorBlanco.sql
+++ Apostila11_NathanLima_VictorBlanco.sql
@@ -191,3 +191,25 @@ BEGIN
     INSERT INTO tb_log (procedimento) VALUES ('sp_obter_notas_para_compor_o_troco');
 END;
 $$;
+-- 1.2 Adicione um procedimento ao sistema do restaurante. Ele deve:
+-- receber um parâmetro de entrada (IN) que representa o código de um cliente 
+-- exibir, com RAISE NOTICE, o total de pedidos que o cliente tem 
+CREATE OR REPLACE PROCEDURE sp_total_pedidos_cliente (
+  IN p_cod_cliente INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  total_pedidos INT;
+BEGIN
+  SELECT COUNT(*)
+  INTO total_pedidos
+  FROM tb_pedido
+  WHERE cod_cliente = p_cod_cliente;
+
+  RAISE NOTICE 'O cliente % possui % pedido(s).', p_cod_cliente, total_pedidos;
+
+  INSERT INTO tb_log (data_execucao, nome_procedure)
+  VALUES (CURRENT_TIMESTAMP, 'sp_total_pedidos_cliente');
+END;
+$$;
