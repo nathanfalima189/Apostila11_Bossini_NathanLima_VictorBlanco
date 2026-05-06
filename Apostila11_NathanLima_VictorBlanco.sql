a19c028d57ba1c657f04f7586516c1123bcb0e9a
RAFAEL <rafaellimapapa898@gmail.com>
2026-05-06 10:37:45 -0300

    Exercício 1.3

--- Apostila11_NathanLima_VictorBlanco.sql
+++ Apostila11_NathanLima_VictorBlanco.sql
@@ -213,3 +213,31 @@ BEGIN
   VALUES (CURRENT_TIMESTAMP, 'sp_total_pedidos_cliente');
 END;
 $$;
+-- 1.3 Reescreva o exercício 1.2 de modo que o total de pedidos seja armazenado em uma variável de saída (OUT). 
+DROP PROCEDURE IF EXISTS sp_total_pedidos_cliente(INTEGER);
+
+CREATE OR REPLACE PROCEDURE sp_total_pedidos_cliente (
+    IN p_cod_cliente INT,
+    OUT total_pedidos INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT COUNT(*) INTO total_pedidos
+    FROM tb_pedido
+    WHERE cod_cliente = p_cod_cliente;
+
+    INSERT INTO tb_log (data_execucao, procedimento)
+    VALUES (CURRENT_TIMESTAMP, 'sp_total_pedidos_cliente');
+END;
+$$;
+
+
+DO $$
+DECLARE
+    total_pedidos INT;
+BEGIN
+    CALL sp_total_pedidos_cliente(1, total_pedidos);
+    RAISE NOTICE 'Total: % pedido(s)', total_pedidos;
+END;
+$$;
